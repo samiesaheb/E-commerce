f869b00c08b4c7bbc813c17cd616ec9ff88a7639
Samie Saheb <shs.priv@gmail.com>
2025-02-14 14:30:16 +0700

    Delete ecommerce/routes directory

--- ecommerce/routes/api/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-
-console.log("📌 API Root Route Loaded: /api");
-
-export async function GET() {
-    console.log("📌 GET request received at /api");
-    return json({ message: "API root is available" });
-}
